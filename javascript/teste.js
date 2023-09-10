c0f4eef93e45e7e22059f0ccc66ffc724ce4304e
RONILDO <124939567+459352578@users.noreply.github.com>
2023-09-10 13:19:06 -0300

    Add files via upload
    modificado dia 10/09

--- javascript/teste.js
+++ javascript/teste.js
@@ -1,4 +1,10 @@
 document.getElementById("demo").innerHTML = "Hello JavaScript";
 
-document.getElementById("demo").style.fontSize = "35px";
+document.getElementById("demo").style.fontSize = "70px";
 document.getElementById("demo").style.display = "block";
+document.getElementById("demo").style.display = "none";
+
+
+document.getElementById("mano").style.fontSize = "70px";
+document.getElementById("mano").style.display = "block";
+document.getElementById("mano").style.display = "none";
